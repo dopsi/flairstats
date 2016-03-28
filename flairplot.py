8cab968f30542bb4d36a5e5fc8d78b1845d59cda
Simon Doppler <dopsi.dev@gmail.com>
2016-03-28 21:05:36 +0200

    Generate a HTML webpage

--- flairplot.py
+++ flairplot.py
@@ -6,8 +6,9 @@ import sys
 import matplotlib.pyplot as plt
 import operator
 from collections import OrderedDict
+import datetime
 
-with open(sys.argv[1]) as f:
+with open(sys.argv[1]+"/flairs.json") as f:
     j = json.load(f)
 
 D = OrderedDict(sorted(j['data'].items(), key=lambda t: t[1]))
@@ -18,4 +19,26 @@ plt.barh(rng, D.values(), align='center')
 plt.yticks(rng, D.keys())
 plt.title("Flairs de r/france par nombre de commentaires")
 
-plt.savefig(sys.argv[2])
+plt.savefig(sys.argv[1]+"/plot.svg")
+
+html_begin = """<!doctype html>
+
+<html lang="en">
+	<head>
+	<meta charset="utf-8">
+
+	<title>The HTML5 Herald</title>
+	<meta name="description" content="Visualisation of r/france comment author's flairs">
+	<meta name="author" content="dopsi">
+
+	</head>
+
+	<body>
+		<img style="width: 100%;" src="plot.svg" alt="Representation"/>
+		<p>Last updated : """
+html_end="""</p>
+	</body>
+</html>"""
+
+with open(sys.argv[1]+"/index.html", "w") as f:
+    f.write(html_begin+str(datetime.datetime.now())+html_end)
