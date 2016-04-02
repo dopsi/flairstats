4dca3911e69d876517bd571a38f7cf5741303ae1
Simon Doppler <dopsi.dev@gmail.com>
2016-04-02 15:04:28 +0200

    Structure of the project

---
+++ flairstats/fetchbot.py
@@ -0,0 +1,41 @@
+from praw import Reddit
+
+import json
+import os
+
+class FetchBot:
+    """Bot to fetch the subreddit data."""
+
+    def __init__(self, user_agent, subreddit, data_file, user=None, password=None):
+        """Basic constructor"""
+        self._user_agent = user_agent
+        self._subreddit = subreddit
+        self._data_file = data_file
+        self._user = user
+        self._password = password
+
+    def fetch(self):
+        """Fetching function"""
+        print('FetchBot called for r/'+self._subreddit)
+
+def FetchBotGenerator(config_file):
+    with open(config_file) as cf:
+        json_config = json.load(cf)
+
+    user_agent = json_config['user-agent']
+    for i in json_config['bots']:
+        yield FetchBot(user_agent, i['subreddit'], i['data-file'])
+
+def run():
+    home = os.getenv('HOME')
+    config_file = os.path.join(home, '.config/flairstats/config.json')
+    if not os.path.exists(config_file):
+        raise FileNotFoundError(config_file)
+
+    fetchbots = FetchBotGenerator(config_file)
+
+    for bot in fetchbots:
+        bot.fetch()
+
+if __name__ == "__main__":
+    run()
---
+++ setup.py
@@ -0,0 +1,30 @@
+import os
+from setuptools import setup
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "flairstats",
+    version = "0.0.1",
+    author = "Simon Doppler",
+    author_email = "dopsi.dev@gmail.com",
+    description = ("Get subreddit statistics"),
+    license = "MIT",
+    keywords = "reddit statistics",
+    url = "http://github.com/dopsi/flairstats",
+    packages=['flairstats'],
+    data_files=[
+        ('share/doc/flairstats/config', ['config/config.json'])
+    ],
+    long_description=read('README.md'),
+    classifiers=[
+        "Development Status :: 2- Pre-Alpha",
+        "Topic :: Text Processing :: Markup :: HTML",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
