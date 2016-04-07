8f1cc65a7a44b4171db0dd6e81d0ca3a134f6471
Simon Doppler <dopsi.dev@gmail.com>
2016-04-07 13:52:03 +0200

    Add top 10 of comments and posts flairs

--- flairstats/statsbot.py
+++ flairstats/statsbot.py
@@ -28,12 +28,27 @@ class StatsBot:
     def generate(self):
         start_time = time.time()
         self._page.header().h1('Subreddit statistics for r/'+self._subreddit)
+
+        section = self._page.section()
         posts_subjects_ranking = OrderedDict(sorted(self._data['posts']['subject-presence'].items(), key=lambda t: t[1], reverse=True))
         del posts_subjects_ranking['None']
-        tbl_posts_subjects_ranking = self._page.section().table()
+        tbl_posts_subjects_ranking = section.article().table()
         tbl_posts_subjects_ranking.tr().td('Flairs de posts les plus utilisés', colspan=2)
         for key, value in posts_subjects_ranking.items():
             tbl_posts_subjects_ranking.tr(value, key)
+
+        posts_flairs_ranking = OrderedDict(sorted(self._data['posts']['flair-presence'].items(), key=lambda t: t[1], reverse=True))
+        tbl_posts_flairs_ranking = section.article().table()
+        tbl_posts_flairs_ranking.tr().td('Flairs des posts les plus utilisés', colspan=2)
+        for key, value in list(posts_flairs_ranking.items())[:10]:
+            tbl_posts_flairs_ranking.tr(value, key)
+
+        comments_flairs_ranking = OrderedDict(sorted(self._data['comments']['flair-presence'].items(), key=lambda t: t[1], reverse=True))
+        tbl_comments_flairs_ranking = section.article().table()
+        tbl_comments_flairs_ranking.tr().td('Flairs des commentaires les plus utilisés', colspan=2)
+        for key, value in list(comments_flairs_ranking.items())[:10]:
+            tbl_comments_flairs_ranking.tr(value, key)
+
         duration = time.time() - start_time
         duration_string = ' (took '+display.float(duration, 3)+' seconds). '
         duration_string += str(self._data['comments']['count'])+' comments analysed, '
