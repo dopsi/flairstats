522f54418df6b37cf6ca0f56b9904c2c02619c2c
Simon Doppler <dopsi.dev@gmail.com>
2016-04-29 14:00:41 +0200

    Use pygal

--- flairstats/statsbot.py
+++ flairstats/statsbot.py
@@ -6,6 +6,8 @@ import htmlgenerator.generator
 from collections import OrderedDict
 import datetime
 import time
+import pygal
+from pygal.style import Style
 
 from .tools import display
 
@@ -15,41 +17,71 @@ class StatsBot:
         self._subreddit = subreddit
         self._data_file = data_file
         self._output_dir = output_dir
+        
+        if not os.path.exists(self._output_dir):
+            os.makedirs(self._output_dir)
+        os.chdir(output_dir)
+        
         with open(self._data_file) as df:
             self._data = json.load(df)
 
         self._page = htmlgenerator.generator.HtmlGenerator()
 
     def __del__(self):
-        if not os.path.exists(self._output_dir):
-            os.makedirs(self._output_dir)
-        self._page.write(os.path.join(self._output_dir, 'index.html'))
+        self._page.write('index.html')
         self._page.title = 'Statistiques de r/'+self._subreddit
     
     def generate(self):
         start_time = time.time()
         self._page.header().h1('Statistiques de r/'+self._subreddit)
 
+# Subject flairs
         section = self._page.section()
-        posts_subjects_ranking = OrderedDict(sorted(self._data['posts']['subject-presence'].items(), key=lambda t: t[1], reverse=True))
+        posts_subjects_ranking = OrderedDict(sorted(self._data['posts']['subject-presence'].items(), key=lambda t: t[1]))
         del posts_subjects_ranking['None']
-        tbl_posts_subjects_ranking = section.article().table()
-        tbl_posts_subjects_ranking.tr().td('Flairs de posts les plus utilisés', colspan=2)
-        for key, value in posts_subjects_ranking.items():
-            tbl_posts_subjects_ranking.tr(value, key)
+        posts_subjects_line_chart = pygal.HorizontalBar()
+        posts_subjects_line_chart.title = 'Subject ranking'
+        posts_subjects_line_chart.x_labels = posts_subjects_ranking.keys()
+        posts_subjects_line_chart.add('Subjects', posts_subjects_ranking.values())
+        with open('posts_subjects_ranking.svg', 'wb') as svgfile:
+            svgfile.write(posts_subjects_line_chart.render())
 
-        posts_flairs_ranking = OrderedDict(sorted(self._data['posts']['flair-presence'].items(), key=lambda t: t[1], reverse=True))
-        tbl_posts_flairs_ranking = section.article().table()
-        tbl_posts_flairs_ranking.tr().td('Flairs des posts les plus utilisés', colspan=2)
-        for key, value in list(posts_flairs_ranking.items())[:10]:
-            tbl_posts_flairs_ranking.tr(value, key)
+        section.embed(src='posts_subjects_ranking.svg', tipe='image/svg+xml', width='50%')
 
-        comments_flairs_ranking = OrderedDict(sorted(self._data['comments']['flair-presence'].items(), key=lambda t: t[1], reverse=True))
-        tbl_comments_flairs_ranking = section.article().table()
-        tbl_comments_flairs_ranking.tr().td('Flairs des commentaires les plus utilisés', colspan=2)
-        for key, value in list(comments_flairs_ranking.items())[:10]:
-            tbl_comments_flairs_ranking.tr(value, key)
+# User flairs
+        section = self._page.section()
+        posts_flairs_ranking = self._data['posts']['flair-presence']
+        comments_flairs_ranking = self._data['comments']['flair-presence']
+        
+        for key in posts_flairs_ranking.keys():
+            if key not in comments_flairs_ranking:
+                comments_flairs_ranking[key]=0
 
+        for key in comments_flairs_ranking.keys():
+            if key not in posts_flairs_ranking:
+                posts_flairs_ranking[key]=0
+
+        for key in comments_flairs_ranking.keys():
+            comments_flairs_ranking[key] /= (self._data['comments']['count']/100)
+
+        for key in posts_flairs_ranking.keys():
+            posts_flairs_ranking[key] /= (self._data['posts']['count']/100)
+
+        custom_style = Style(
+                label_font_size=6,
+                tooltip_font_size=7)
+
+        user_flairs_line_chart = pygal.HorizontalBar(height=12*len(posts_flairs_ranking), style=custom_style)
+        user_flairs_line_chart.title = 'Subject ranking'
+        user_flairs_line_chart.x_labels = posts_flairs_ranking.keys()
+        user_flairs_line_chart.add('Posts', posts_flairs_ranking.values())
+        user_flairs_line_chart.add('Comments', comments_flairs_ranking.values())
+        with open('user_flairs_ranking.svg', 'wb') as svgfile:
+            svgfile.write(user_flairs_line_chart.render())
+
+        section.embed(src='user_flairs_ranking.svg', tipe='image/svg+xml')
+
+# Footer
         duration = time.time() - start_time
         duration_string = ' (en '+display.float(duration, 3)+' secondes). '
         duration_string += str(self._data['comments']['count'])+' commentaires analysés, '
@@ -58,7 +90,7 @@ class StatsBot:
         foot.append(' Code source disponible sur ')
         foot.append(htmlgenerator.markup.HtmlAnchor('GitHub', 'http://github.com/dopsi/flairstats'))
         foot.append('.')
-
+        
 def StatsBotGenerator(config_file):
     """Generate a list-like container of StatsBot objects"""
     with open(config_file) as cf:
