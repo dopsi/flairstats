572d8b9103bd3f36c0eafde73fd9ba91d307b3e8
Simon Doppler <dopsi@dopsi.ch>
2016-09-25 22:36:14 +0530

    Fix title

--- flairstats/statsbot.py
+++ flairstats/statsbot.py
@@ -60,7 +60,7 @@ class StatsBot:
         # Weekly activity
 
         week_activity = pygal.Radar()
-        week_activity.title = 'Activité en fonction de l\'heure'
+        week_activity.title = 'Activité en fonction du jour'
 
         weekdays = OrderedDict([(0, "Monday"), (1, "Tuesday"), (2, "Wednesday"), (3, "Thursday"), (4, "Friday"), (5, "Saturday"),(6, "Sunday")])
 
